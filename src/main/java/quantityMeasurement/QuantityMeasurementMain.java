9f0f88b449eb2ff8bf76e6e4d3376633400ba016
Sheralram <ramkrishna.sheral@gmail.com>
2021-10-29 16:31:49 +0530

    Added: Performed Uc2 Test to check 3Feet = 1Yard

--- src/main/java/quantityMeasurement/QuantityMeasurementMain.java
+++ src/main/java/quantityMeasurement/QuantityMeasurementMain.java
@@ -3,14 +3,17 @@ package quantityMeasurement;
 public class QuantityMeasurementMain {
     public boolean inchConversion(double feet, int inch) {
         double result = feet * 12;
-        if (result == inch)
-        {
-            return  true;
+        if (result == inch) {
+            return true;
         }
         return false;
     }
 
     public boolean yardConversion(double feet, double yard) {
+        double result = yard * 3;
+        if (result == feet) {
+            return true;
+        }
         return false;
     }
 }
