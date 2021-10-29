7828665bcb045054dec213195df360e9e51425ac
Sheralram <ramkrishna.sheral@gmail.com>
2021-10-29 13:25:32 +0530

    Added : Perormed Test for Null Check of Feet

--- src/test/java/quantityMeasurement/QuantityTest.java
+++ src/test/java/quantityMeasurement/QuantityTest.java
@@ -6,23 +6,24 @@ import org.junit.jupiter.api.Test;
 public class QuantityTest {
 
     @Test
-    void given0FeetAnd0Feet_ShouldReturnEqual() {
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assertions.assertEquals(feet1, feet2);
     }
 
     @Test
-    void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
         Assertions.assertNotEquals(feet1, feet2);
     }
 
-    @Test
-    void givenNullFeet_WhenCompare_ShouldReturnFalse() {
+    @Test                                                                 //NULL CHECK
+    public void givenNullFeet_WhenCompare_ShouldReturnFalse() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = null;
-        Assertions.assertEquals(feet1, feet2);
+        Assertions.assertNotEquals(feet1, feet2);
     }
+
 }
